f51dbc79b0c463459b6fc13ef21ae37fcd10d7fc
Exalm <exalm7659@gmail.com>
2017-05-25 13:38:37 +0500

    Removed X11 gesture hack

--- README.md
+++ README.md
@@ -6,8 +6,4 @@ meaning that the scrolling workspaces stick to fingers, so that the animation is
 It currently uses a 3-finger swipe rather than a 4-finger one because my touchpad only supports 3 fingers.
 Perhaps this should be configurable.
 
-The extension supports the gesture even on X11 via parsing libinput-debug-events output.
-For that the current user has to be in input group.
-This is pretty fragile, and I may remove it later. On Wayland native gesture recognition is used.
-
 Both vertical (default) and horizontal workspaces are supported. Multi-monitor configurations are also supported.
--- workspace_switch_gesture@exalm/extension.js
+++ workspace_switch_gesture@exalm/extension.js
@@ -6,7 +6,6 @@ const ExtensionUtils = imports.misc.extensionUtils;
 const Ext = ExtensionUtils.getCurrentExtension();
 
 const Gesture = Ext.imports.gesture;
-const X11GestureListener = Ext.imports.x11GestureListener;
 
 let _gesture;
 let _event;
@@ -16,19 +15,11 @@ function init() {
 
 function enable() {
   _gesture = new Gesture.TouchpadWorkspaceSwitchAction();
-
-  if (Meta.is_wayland_compositor())
-    _event = global.stage.connect('captured-event', Lang.bind(_gesture, _gesture._handleEvent));
-  else
-    X11GestureListener.start(Lang.bind(_gesture, _gesture._handleEvent));
+  _event = global.stage.connect('captured-event', Lang.bind(_gesture, _gesture._handleEvent));
 }
 
 function disable() {
-  if (Meta.is_wayland_compositor())
-    global.stage.disconnect(_event);
-  else
-    X11GestureListener.stop();
-
+  global.stage.disconnect(_event);
   _gesture = null;
   _event = null;
 }
