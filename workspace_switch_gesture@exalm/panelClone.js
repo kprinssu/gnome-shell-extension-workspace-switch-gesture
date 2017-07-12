d509c9c946e7047cd49f608d747c2fb6f19856e0
Exalm <exalm7659@gmail.com>
2017-07-12 16:03:03 +0500

    Moved fakeAppMenu management to PanelClone

--- workspace_switch_gesture@exalm/panelClone.js
+++ workspace_switch_gesture@exalm/panelClone.js
@@ -1,22 +1,79 @@
 const Clutter = imports.gi.Clutter;
 const Lang = imports.lang;
+const Main = imports.ui.main;
+const Meta = imports.gi.Meta;
+const Shell = imports.gi.Shell;
+const St = imports.gi.St;
+
+const ExtensionUtils = imports.misc.extensionUtils;
+const Ext = ExtensionUtils.getCurrentExtension();
+const FakeAppMenu = Ext.imports.fakeAppMenu;
 
 const PanelClone = new Lang.Class({
   Name: 'PanelClone',
 
-  _init: function(actor, mon) {
-    [anchorX, anchorY] = actor.get_anchor_point();
+  _init: function(ws, mon, panel) {
+    this.monitor = mon;
+    this.workspace = ws;
+
+    this.actor = new St.Widget();
 
     this._clone = new Clutter.Clone({
-      source: actor,
-      x: actor.x - mon.x,
-      y: actor.y - mon.y
+      source: panel,
+      x: panel.x - mon.x,
+      y: panel.y - mon.y
     });
-    this._clone.set_anchor_point(anchorX, anchorY);
+    this._clone.set_anchor_point(panel.anchor_x, panel.anchor_y);
+
+    this.actor.add_child(this._clone);
+
+    if (panel == Main.panel.actor.get_parent() &&
+        this.monitor.index == global.screen.get_primary_monitor()) {
+      // Create fake workspace-specific appmenu
+      let app = this._getFocusedApp();
+      if (app != null && !this._fullscreen) {
+        this._fakeAppMenu = new FakeAppMenu.FakeAppMenuButton(app, this.actor);
+      }
+    }
   },
 
   destroy: function() {
+    if (this._fakeAppMenu) {
+      this._fakeAppMenu.destroy();
+      this._fakeAppMenu = null;
+    }
     this._clone.destroy();
-    this._clone = null;
+    this.actor.destroy();
+  },
+
+  _getFocusedApp: function() {
+    let tracker = Shell.WindowTracker.get_default();
+
+    // If this is the active workspace, just request the focused app normally
+    if (this.workspace == global.screen.get_active_workspace()) {
+      let focusedApp = tracker.focus_app;
+      if (focusedApp && focusedApp.is_on_workspace(this.workspace))
+        return focusedApp;
+    }
+
+    let windows = global.get_window_actors();
+
+    // Take out the ones on other workspaces and monitors
+    windows = windows.filter(actor => {
+      let window = actor.get_meta_window();
+      return window.located_on_workspace(this.workspace) &&
+             window.get_monitor() == this.monitor.index &&
+             window.showing_on_its_workspace() &&
+             window.get_window_type() != Meta.WindowType.OVERRIDE_OTHER &&
+             window.get_window_type() != Meta.WindowType.DESKTOP;
+    });
+
+    // Get the app for the topmost window
+    if (windows.length > 0) {
+      let win = windows[windows.length - 1].get_meta_window();
+      return tracker.get_window_app(win);
+    }
+
+    return null;
   }
 });
--- workspace_switch_gesture@exalm/workspaceClone.js
+++ workspace_switch_gesture@exalm/workspaceClone.js
@@ -7,7 +7,6 @@ const St = imports.gi.St;
 
 const ExtensionUtils = imports.misc.extensionUtils;
 const Ext = ExtensionUtils.getCurrentExtension();
-const FakeAppMenu = Ext.imports.fakeAppMenu;
 const PanelClone = Ext.imports.panelClone;
 
 const WindowClone = new Lang.Class({
@@ -20,7 +19,6 @@ const WindowClone = new Lang.Class({
       x: actor.x - mon.x,
       y: actor.y - mon.y
     });
-//    this._clone.set_clip(0, 0, actor.get_width(), actor.get_height());
   },
 
   destroy: function() {
@@ -94,25 +92,16 @@ const WorkspaceClone = new Lang.Class({
     });
 
     // Also add panels
-    // FIXME: Do we have to distinguish these ones and windows
     this._panels = [];
     if (!this.fullscreen) {
       let panels = Main.layoutManager._trackedActors.filter(data => {
           return data.trackFullscreen;
       });
       panels.forEach(data => {
-        let clone = new PanelClone.PanelClone(data.actor, this._monitor);
-        this._container.add_child(clone._clone);
+        let clone = new PanelClone.PanelClone(this.workspace, this._monitor, data.actor);
+        this._container.add_child(clone.actor);
         this._panels.push(clone);
       });
-
-      if (this._monitor.index == global.screen.get_primary_monitor()) {
-        // Create fake workspace-specific appmenu
-        let app = this._getFocusedApp();
-        if (app != null && !this._fullscreen) {
-          this._fakeAppMenu = new FakeAppMenu.FakeAppMenuButton(app, this._container);
-        }
-      }
     }
   },
 
@@ -132,12 +121,6 @@ const WorkspaceClone = new Lang.Class({
     this._bg.destroy();
     this._bg = null;
 
-    // Destroy fake appmenu
-    if (this._fakeAppMenu) {
-      this._fakeAppMenu.destroy();
-      this._fakeAppMenu = null;
-    }
-
     this._container.destroy();
     this._container = null;
 
@@ -150,37 +133,6 @@ const WorkspaceClone = new Lang.Class({
 
     this.workspace = null;
     this._monitor = null;
-  },
-
-  _getFocusedApp: function() {
-    let tracker = Shell.WindowTracker.get_default();
-
-    // If this is the active workspace, just request the focused app normally
-    if (this.workspace == global.screen.get_active_workspace()) {
-      let focusedApp = tracker.focus_app;
-      if (focusedApp && focusedApp.is_on_workspace(this.workspace))
-        return focusedApp;
-    }
-
-    let windows = global.get_window_actors();
-
-    // Take out the ones on other workspaces and monitors
-    windows = windows.filter(actor => {
-      let window = actor.get_meta_window();
-      return window.located_on_workspace(this.workspace) &&
-             window.get_monitor() == this._monitor.index &&
-             window.showing_on_its_workspace() &&
-             window.get_window_type() != Meta.WindowType.OVERRIDE_OTHER &&
-             window.get_window_type() != Meta.WindowType.DESKTOP;
-    });
-
-    // Get the app for the topmost window
-    if (windows.length > 0) {
-      let win = windows[windows.length - 1].get_meta_window();
-      return tracker.get_window_app(win);
-    }
-
-    return null;
   }
 });
 
